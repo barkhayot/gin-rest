77b7db66b7f4af234ed17999e872288346490133
barkhayot <barkhayotoff@gmail.com>
2024-01-07 14:10:47 +0900

    commit

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Define struct of data
+type album struct {
+	ID       string  `json:"id"`
+	Title    string  `json:"title"`
+	Artist   string  `json:"artist"`
+	Price    float64 `json:"price"`
+	Quantity int     `json:"quantity"`
+}
+
+// Use as memery storage
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99, Quantity: 12},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99, Quantity: 3},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99, Quantity: 99},
+}
+
+// helpers
+
+func albumById(id string) (*album, error) {
+	for i, a := range albums {
+		if a.ID == id {
+			return &albums[i], nil
+		}
+	}
+	return nil, errors.New("Album not found")
+}
+
+// Route funcitons
+
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+	album, err := albumById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album by ID not found"})
+		return
+	}
+	c.IndentedJSON(http.StatusOK, album)
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	res := map[string]string{"message": "created"}
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid Body"})
+		return
+	}
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, res)
+}
+
+func checkoutAlbum(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if ok == false {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "missing query ID param"})
+	}
+
+	album, err := albumById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album by ID not found"})
+		return
+	}
+
+	if album.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Abbum is out of stock"})
+		return
+	}
+
+	album.Quantity -= 1
+	c.IndentedJSON(http.StatusOK, album)
+
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumById)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Gin REST API
+
+Build REST API with Gin 
+
+## Introduction
+
+Simple usaga of Gin framework for REST api implementation
+
+
+### API Endpoints
+`GET /albums`: Get all albums.
+`GET /albums/:id`: Get details of an album.
+`POST /albums`: Create a new album.
+`GET /checkout?id=<album_id>`: Checkout an album.
